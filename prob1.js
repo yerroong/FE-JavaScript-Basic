9cb3fff7923b478d3678035170cef44a711937ab
yerin <wbflqldks90@naver.com>
2024-04-02 22:11:19 +0900

    1번 어른사자 완료

--- prob1.js
+++ prob1.js
@@ -1,5 +1,9 @@
 /* 1번 문제 답안을 작성해주세요. */
+// function solution(num1, num2) {
+//   var answer = num1 * num2;
+//   return answer;
+// } 
+
 function solution(num1, num2) {
-  var answer = num1 * num2;
-  return answer;
+  return [num1, num2].map(x => x * num2)[0];
 }
